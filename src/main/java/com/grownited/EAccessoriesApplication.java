5f3f7801b62aee0ab056d20aa312c6ad93c7c6cb
divy121 <divychaudhari234@gmail.com>
2025-02-10 14:49:17 +0530

    init

---
+++ src/main/java/com/grownited/EAccessoriesApplication.java
@@ -0,0 +1,13 @@
+package com.grownited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAccessoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAccessoriesApplication.class, args);
+	}
+
+}
